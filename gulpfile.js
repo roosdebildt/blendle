57f349a128be5734f7885c1678a874fa985a6001
Roos de Bildt <roosdebildt@gmail.com>
2015-12-15 12:56:23 +0100

    basic page content

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,41 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var connect = require('gulp-connect');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
 // compile sass files synchronously
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
 gulp.task('sass', function () {
   gulp.src('./app/stylesheets/sass/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(sass.sync({
+      includePaths: require('node-reset-scss').includePath
+    })
+    .on('error', sass.logError))
     .pipe(gulp.dest('./app/stylesheets/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./app/stylesheets/sass/**/*.scss', ['sass']);
 });
 
 // watch and log changes in files
 gulp.task('watch', function(){
-  var watcher = gulp.watch(['app/*.html', './app/stylesheets/sass/**/*.scss']);
+  var watcher = gulp.watch(['app/*.html', './app/stylesheets/sass/**/*.scss', './app/stylesheets/css/**/*.css']);
   watcher.on('change', function(event){
     console.log('File: ' + event.path + ' was changed.');
   });
 });
 
-gulp.task('connect'), connect.server({
-  root: ['app']
+// live reload
+gulp.task('serve', function() {
+  browserSync.init({
+    server: "./app"
+  });
+
+  gulp.watch("./app/stylesheets/sass/*.scss");
+  gulp.watch("./app/stylesheets/sass/*.scss").on('change', reload);
 });
 
-gulp.task('default', ['sass', 'connect', 'watch']);
+gulp.task('default', ['sass', 'watch', 'serve']);
